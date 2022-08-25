8f53f66f3becbbee04914977665ad49e11efb209
corbincargil <corbin.cargil@gmail.com>
2022-08-24 20:03:06 -0500

    added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,16 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
